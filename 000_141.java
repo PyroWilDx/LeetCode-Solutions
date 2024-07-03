0a51571400d6c9ac713610bad9c1cb596fff092c
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-07-03 22:07:56 +0200

    Day 19

---
+++ 000_141.java
@@ -0,0 +1,28 @@
+class Solution {
+    class ListNode {
+        int val;
+        ListNode next;
+        
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        
+        ListNode slow = head;
+        ListNode fast = slow.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return false;
+    }
+}
