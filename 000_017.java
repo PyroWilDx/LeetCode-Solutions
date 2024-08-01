0903ca199297f5c27ec165a273d0a6027b708240
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-08-01 23:07:55 +0200

    Day 28

---
+++ 000_017.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private char[][] numsMap = new char[][] {
+        {},
+        {'a', 'b', 'c'},
+        {'d', 'e', 'f'},
+        {'g', 'h', 'i'},
+        {'j', 'k', 'l'},
+        {'m', 'n', 'o'},
+        {'p', 'q', 'r', 's'},
+        {'t', 'u', 'v'},
+        {'w', 'x', 'y', 'z'}
+    };
+
+    String digits;
+    private List<String> result;
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return new ArrayList<>();
+
+        this.digits = digits;
+
+        result = new ArrayList<>();
+        StringBuilder l = new StringBuilder();
+        letterCombinationsRec(0, l);
+        return result;
+    }
+
+    public void letterCombinationsRec(int index, StringBuilder l) {
+        if (index == digits.length()) {
+            result.add(l.toString());
+            return;
+        }
+
+        char[] chars = numsMap[digits.charAt(index) - '0' - 1];
+        for (int i = 0; i < chars.length; i++) {
+            l.append(chars[i]);
+            letterCombinationsRec(index + 1, l);
+            l.deleteCharAt(l.length() - 1);
+        }
+    }
+}
---
+++ 000_131.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private String s;
+    private char[] sArray;
+    private int N;
+    private List<List<String>> result;
+
+    public List<List<String>> partition(String s) {
+        this.s = s;
+        this.sArray = s.toCharArray();
+        this.N = s.length();
+
+        result = new ArrayList<>();
+
+        List<String> l = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            partitionRec(0, i + 1, l);
+        }
+  
+        return result;
+    }
+
+    public void partitionRec(int start, int end, List<String> l) {
+        if (!isPalindrome(start, end)) {
+            return;
+        }
+
+        if (end == N) {
+            List<String> r = new ArrayList<>(l);
+            r.add(s.substring(start, end));
+            result.add(r);
+            return;
+        }
+
+        l.add(s.substring(start, end));
+        for (int i = end; i < N; i++) {
+            partitionRec(end, i + 1, l);
+        }
+        l.remove(l.size() - 1);
+    }
+
+    private boolean isPalindrome(int start, int end) {
+        end--;
+        while (start < end) {
+            if (sArray[start] != sArray[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
