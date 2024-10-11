f6897828d48d71f20fecb68f147e6d7bf4de8f72
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-10-11 14:10:22 +0200

    Day 51

---
+++ 001_133.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestUniqueNumber(int[] nums) {
+        int[] numsCount = new int[1001];
+        for (int num : nums) {
+            numsCount[num]++;
+        }
+        int result = -1;
+        for (int i = numsCount.length - 1; i >= 0; i--) {
+            if (numsCount[i] == 1) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
